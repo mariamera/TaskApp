19edaa21228669bb43c69f683d5006faf28dc014
Maria Mera <mmera@intellisys.com.do>
2018-08-11 10:29:44 -0400

    wip

--- app/src/main/java/com/example/mariaadelaidameramiguens/taskapp/vista/LogInActivity.java
+++ app/src/main/java/com/example/mariaadelaidameramiguens/taskapp/vista/LogInActivity.java
@@ -10,6 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.mariaadelaidameramiguens.taskapp.MainActivity;
 import com.example.mariaadelaidameramiguens.taskapp.R;
 import com.example.mariaadelaidameramiguens.taskapp.entitdades.DataHolder;
@@ -35,32 +42,65 @@ public class LogInActivity extends AppCompatActivity {
         final TextView errorMsj =   (TextView) findViewById(R.id.errorMsg);
         final EditText usuarioUsername = (EditText)findViewById(R.id.usuarioLogInText);
         final EditText usuarioContrasena = (EditText) findViewById(R.id.usuarioLogInContrasena);
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url ="192.168.200.150:8080/registrar";
 
         logInRepositorio = new LogInRepositorioImp(this);
 
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Usuario usuario =  logInRepositorio.logIn(usuarioUsername.getText().toString(), usuarioContrasena.getText().toString() );
-                if(usuario.getId() != null) {
-                    Intent intent1 = null;
 
-                    if( usuario.getTipoUsuario() == Usuario.TipoUsuario.TECNICO) {
-                         intent1 = new Intent(LogInActivity.this, UsuarioActivity.class);
+                final Usuario usuario =  new Usuario();
+                usuario.setEmail("maria@me.com");
+                usuario.setNombre("maria adelaida mera");
+                usuario.setContrasena("1234");
+                usuario.setTipoUsuario(Usuario.TipoUsuario.TECNICO);
+                errorMsj.setText("HERe");
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                errorMsj.setText("Response");
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        errorMsj.setText("That didn't work!");
                     }
+                }){
+                    @Override
+                    public byte[] getBody() throws AuthFailureError {
+                        Gson gson = new Gson();
+                        String json = gson.toJson(usuario);
 
-                    if (usuario.getTipoUsuario() == Usuario.TipoUsuario.NORMAL) {
-                         intent1 = new Intent(LogInActivity.this, UsuarioNormalActivity.class);
+                        return json.getBytes();
                     }
-
-                    DataHolder.getInstance().setData(usuario);
-                    intent1.putExtra("usuario", usuario); // pasar parametros para un activity.
-
-                    startActivity(intent1);
-
-                } else {
-                        Toast.makeText(LogInActivity.this, "Usuario No Encontrado", Toast.LENGTH_LONG).show();
-                }
+                };
+        queue.add(stringRequest);
+//                Usuario usuario =  logInRepositorio.logIn(usuarioUsername.getText().toString(), usuarioContrasena.getText().toString() );
+//                if(usuario.getId() != null) {
+//                    Intent intent1 = null;
+//
+//                    if( usuario.getTipoUsuario() == Usuario.TipoUsuario.TECNICO) {
+//                         intent1 = new Intent(LogInActivity.this, UsuarioActivity.class);
+//                    }
+//
+//                    if (usuario.getTipoUsuario() == Usuario.TipoUsuario.NORMAL) {
+//                         intent1 = new Intent(LogInActivity.this, UsuarioNormalActivity.class);
+//                    }
+//
+//                    DataHolder.getInstance().setData(usuario);
+//                    intent1.putExtra("usuario", usuario); // pasar parametros para un activity.
+//
+//                    startActivity(intent1);
+//
+//                } else {
+//                        Toast.makeText(LogInActivity.this, "Usuario No Encontrado", Toast.LENGTH_LONG).show();
+//                }
 
             }
         });
